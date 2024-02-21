9a8c24f6a836692267f12f45e8af0dbc019ca9a4
Sherlin-2002 <159281169+Sherlin-2002@users.noreply.github.com>
2024-02-21 21:29:28 +0530

    Add files via upload

---
+++ DirectoryContents.java
@@ -0,0 +1,74 @@
+import java.io.File;
+
+ 
+
+public class DirectoryContents {
+
+    public static void main(String[] args) {
+
+      
+
+        String directoryPath = "D:/javaprgs";
+
+ 
+
+       
+
+        File directory = new File(directoryPath);
+
+ 
+
+       
+
+        if (directory.exists() && directory.isDirectory()) {
+
+            displayContents(directory);
+
+        } else {
+
+            System.out.println("Invalid directory path.");
+
+        }
+
+    }
+
+ 
+
+    public static void displayContents(File directory) {
+
+       
+
+        File[] files = directory.listFiles();
+
+ 
+
+       
+
+        for (File file : files) {
+
+            if (file.isDirectory()) {
+
+                System.out.println("Subdirectory: " + file.getName());
+
+            }
+
+        }
+
+ 
+
+      
+
+        for (File file : files) {
+
+            if (file.isFile()) {
+
+                System.out.println("File: " + file.getName());
+
+            }
+
+        }
+
+    }
+
+}
+
